08942e20827966431be7919d3d272b67cd68ea3e
Jay M Gonzalez <jaimungon@gmail.com>
2021-02-26 22:51:38 +0100

    vage lite viz

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App.fetchData'
+import App from './vegaLite/App.vega'
 
 ReactDOM.render(
   <App />,
